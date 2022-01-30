e04f05d6b23c1f71786d7611e29619894e5a8e0a
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-01-30 21:40:38 +0300

    shows that it is not enough to mock public properties

--- src/back-up-path.test.ts
+++ src/back-up-path.test.ts
@@ -3,7 +3,13 @@ import { getBackupPath } from './back-up-path';
 import { expect } from 'chai';
 
 describe('поведение back-up-path', () => {
-  const getMockSettingsManager = (): SettingsManager => { throw new Error('not implemented yet'); }
+  const getMockSettingsManager = (): SettingsManager => { 
+    const result: SettingsManager = {
+      name: 'test1',
+      path: 'test2',      
+    }
+    return result;
+   }
   it('добавляет название файла к базовому пути', () => {
     const mockSettingsManager = getMockSettingsManager()
     const backupPath = getBackupPath(mockSettingsManager);
